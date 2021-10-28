8cebafae3aeb67bf94644f13a94a7c7d297cfe6c
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-10-28 21:49:37 +0300

    upd

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,5 @@
 export default function fetchCountries(searchQuery) {
-  const url = `https://restcountries.com/v2/name/${searchQuery}`;
-
-  return fetch(url)
+  return fetch(`https://restcountries.com/v2/name/${searchQuery}`)
     .then(response => response.json())
     .then(data => data);
 }
